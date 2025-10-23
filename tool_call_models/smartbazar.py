e563b67e6638a79fdfb222c732a312583e8ffa2a
aslon <aslon@adsfasdjkl/cp.com>
2025-10-23 05:47:29 -0400

    new api request model

--- tool_call_models/smartbazar.py
+++ tool_call_models/smartbazar.py
@@ -68,26 +68,28 @@ class Offer(BaseModel):
 
     merchant: Optional[Merchant] = None
     status: Optional[Dict[str, Any]] = None
-    market_type: Optional[Literal[
-        "b2c",
-        "b2b",
-        "B2B",
-        "B2C",
-        "G2B",
-        "G2C",
-        "C2C",
-        "C2B",
-        "B2G",
-        "G2G",
-        "G2C",
-        "g2b",
-        "g2c",
-        "c2c",
-        "c2b",
-        "b2g",
-        "g2g",
-        "g2c",
-    ]] = None
+    market_type: Optional[
+        Literal[
+            "b2c",
+            "b2b",
+            "B2B",
+            "B2C",
+            "G2B",
+            "G2C",
+            "C2C",
+            "C2B",
+            "B2G",
+            "G2G",
+            "G2C",
+            "g2b",
+            "g2c",
+            "c2c",
+            "c2b",
+            "b2g",
+            "g2g",
+            "g2c",
+        ]
+    ] = None
 
     def filter_for_llm(self):
         return {
@@ -95,9 +97,9 @@ class Offer(BaseModel):
             "merchant": self.merchant.name if self.merchant else None,
             "status": self.status if self.status else None,
             "six_month_price": self.six_month_price if self.six_month_price else None,
-            "twelve_month_price": self.twelve_month_price
-            if self.twelve_month_price
-            else None,
+            "twelve_month_price": (
+                self.twelve_month_price if self.twelve_month_price else None
+            ),
         }
 
 
@@ -122,13 +124,19 @@ class Price(BaseModel):
     price: Optional[Union[str, int, float]] = None
     monthly: Optional[Union[str, int, float]] = None
     installments: Optional[list[Installment]] = None
+
     def filter_for_llm(self):
         return {
             "original": self.original if self.original else None,
             "price": self.price if self.price else None,
             "monthly": self.monthly if self.monthly else None,
-            "installments": [x.model_dump() for x in self.installments] if self.installments else None,
+            "installments": (
+                [x.model_dump() for x in self.installments]
+                if self.installments
+                else None
+            ),
         }
+
 
 class ProductItem(BaseModel):
     id: Optional[Union[int, str]] = None
@@ -165,3 +173,95 @@ class SearchProductsResponse(BaseToolCallModel, BaseModel):
     def filter_for_llm(self):
         data = [x.filter_for_llm() for x in self.products] if self.products else []
         return json.dumps(data, ensure_ascii=False, indent=2)
+
+
+class CartItemPrice(BaseModel):
+    quantity: Optional[int] = None
+    original: Optional[Union[str, int, float]] = None
+    total_price: Optional[Union[str, int, float]] = None
+    monthly: Optional[Union[str, int, float]] = None
+
+
+class CartItem(BaseModel):
+    offer_id: Optional[Union[int, str]] = None
+    product_id: Optional[Union[int, str]] = None
+    name: Optional[str] = None
+    slug: Optional[str] = None
+    remote_id: Optional[str] = None
+    image: Optional[str] = None
+    quantity: Optional[int] = None
+    price: Optional[CartItemPrice] = None
+
+    def filter_for_llm(self):
+        return {
+            "name": self.name if self.name else None,
+            "quantity": self.quantity if self.quantity else None,
+            "price": self.price.filter_for_llm() if self.price else None,
+        }
+
+
+class Shipping(BaseModel):
+    method: Optional[int] = None
+    price: Optional[Union[str, int, float]] = None
+    address_id: Optional[Union[int, str]] = None
+    address: Optional[str] = None
+    pick_point_id: Optional[str] = None
+
+
+class CartMerchant(BaseModel):
+    id: Optional[Union[int, str]] = None
+    name: Optional[str] = None
+    slug: Optional[str] = None
+    logo: Optional[str] = None
+    items: Optional[list[CartItem]] = None
+    shipping: Optional[Shipping] = None
+
+    def filter_for_llm(self):
+        return {
+            "id": self.id if self.id else None,
+            "name": self.name if self.name else None,
+            "items": [x.filter_for_llm() for x in self.items] if self.items else None,
+            # "shipping": self.shipping.filter_for_llm() if self.shipping else None, # disable shipping info for now
+        }
+
+
+class CartTotal(BaseModel):
+    total_product_quantity: Optional[int] = None
+    total_product_price: Optional[Union[str, int, float]] = None
+    total_insurance_price: Optional[Union[str, int, float]] = None
+    total_price: Optional[Union[str, int, float]] = None
+    installment_period: Optional[int] = None
+    total_monthly_installment_price: Optional[Union[str, int, float]] = None
+    total_shipping_price: Optional[Union[str, int, float]] = None
+
+    def filter_for_llm(self):
+        return {
+            "total_product_quantity": (
+                self.total_product_quantity if self.total_product_quantity else None
+            ),
+            "total_product_price": (
+                self.total_product_price if self.total_product_price else None
+            ),
+        }
+
+
+class CartData(BaseModel):
+    merchants: Optional[list[CartMerchant]] = None
+    total: Optional[CartTotal] = None
+
+    def filter_for_llm(self):
+        return {
+            "merchants": (
+                [x.filter_for_llm() for x in self.merchants] if self.merchants else None
+            ),
+            "total": self.total.filter_for_llm() if self.total else None,
+        }
+
+
+class CartResponse(BaseToolCallModel, BaseModel):
+    status: Optional[int] = None
+    message: Optional[str] = None
+    data: Optional[CartData] = None
+
+    def filter_for_llm(self):
+        return {"data": self.data.filter_for_llm() if self.data else None}
